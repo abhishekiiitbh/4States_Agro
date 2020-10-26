919f514f08e2ef06c0d3f35d23cb348e9f729820
Guna-git <gunasowdesh@gmail.com>
2020-10-26 22:54:26 +0530

    Commit Bidder FrontEnd

---
+++ web/js/popup.js
@@ -0,0 +1,15 @@
+$(document).ready(function() { 
+              
+    /* Centering the modal vertically */ 
+    function alignModal() { 
+        var modalDialog = $(this).find(".modal-dialog"); 
+        modalDialog.css("margin-top", Math.max(0,  
+        ($(window).height() - modalDialog.height()) / 2)); 
+    } 
+    $(".modal").on("shown.bs.modal", alignModal); 
+
+    /* Resizing the modal according the screen size */ 
+    $(window).on("resize", function() { 
+        $(".modal:visible").each(alignModal); 
+    }); 
+}); 
